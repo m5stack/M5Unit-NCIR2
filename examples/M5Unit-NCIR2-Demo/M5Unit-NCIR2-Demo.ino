9f4a34e8fd3f3152bfe1f2fa5cca42410d3b8c28
chenzhjie <jaychen@m5stack.com>
2022-10-11 14:28:29 +0800

    Update M5Unit-NCIR2-Demo.ino

--- examples/M5Unit-NCIR2-Demo/M5Unit-NCIR2-Demo.ino
+++ examples/M5Unit-NCIR2-Demo/M5Unit-NCIR2-Demo.ino
@@ -31,17 +31,6 @@ uint16_t low_alarm_freq, high_alarm_freq, low_alarm_interval,
 uint8_t low_alarm_duty, high_alarm_duty, duty;
 int16_t low_alarm_temp, high_alarm_temp;
 
-enum {
-    D_WRITE_TEST = 0,
-    D_READ_TEST,
-    PWM_TEST,
-    ADC_TEST,
-    RGB_TEST,
-    SERVO_TEST,
-    SERVO_TEST_AUTO,
-    MULTI_TEST
-};
-
 void setup() {
     M5.begin(true, false, true);
     M5.Power.begin();
@@ -50,8 +39,6 @@ void setup() {
     canvas.setColorDepth(8);  // mono color
     canvas.setFont(&fonts::efontCN_12);
     canvas.createSprite(display.width(), display.height());
-    // ncir2.setEmissivity(0.95);
-    emissivity = 0.95;
     emissivity = ncir2.getEmissivity();
 
     ncir2.setLEDColor(0x001100);
@@ -76,6 +63,8 @@ void setup() {
     ncir2.getAlarmTemp(M5_NCIR_ALARM_LOW, &low_alarm_temp);
     ncir2.setAlarmTemp(M5_NCIR_ALARM_HIGH, 3700);
     ncir2.getAlarmTemp(M5_NCIR_ALARM_HIGH, &high_alarm_temp);
+
+    ncir2.setConfig();
 }
 
 void loop() {
@@ -87,64 +76,34 @@ void loop() {
     canvas.setTextSize(2);
     canvas.setCursor(100, 0);
     canvas.setTextColor(YELLOW);
-    canvas.printf("NCIR2 Test");
+    canvas.printf("NCIR2 Demo");
     canvas.setTextColor(ORANGE);
     canvas.drawLine(25, 25, 310, 25);
     canvas.setCursor(0, 30);
     canvas.setTextColor(WHITE);
-    canvas.printf("温度:");
+    canvas.printf("Temp:");
     canvas.setTextColor(GREEN);
     canvas.printf("%d", temperature);
     canvas.setCursor(0, 60);
     canvas.setTextColor(RED);
-    canvas.printf("高温:%d", high_alarm_temp);
+    canvas.printf("HighAlarm:%d ", high_alarm_temp);
     canvas.setTextColor(BLUE);
-    canvas.printf("低温:%d", low_alarm_temp);
-    canvas.setCursor(0, 90);
-    canvas.setTextColor(WHITE);
-    canvas.printf("发射率:");
-    canvas.setTextColor(GREEN);
-    canvas.printf("%.2f", emissivity);
+    canvas.printf("LowAlarm:%d", low_alarm_temp);
     canvas.setCursor(0, 120);
     canvas.setTextColor(WHITE);
-    canvas.printf("芯片温度:");
+    canvas.printf("Emissivity:");
     canvas.setTextColor(GREEN);
-    canvas.printf("%d", temperature_soc);
+    canvas.printf("%.2f", emissivity);
     canvas.setCursor(0, 150);
     canvas.setTextColor(WHITE);
-    canvas.printf("按钮");
+    canvas.printf("SocTemp:");
+    canvas.setTextColor(GREEN);
+    canvas.printf("%d", temperature_soc);
+    canvas.setCursor(0, 180);
+    canvas.setTextColor(WHITE);
+    canvas.printf("Button:");
     canvas.setTextColor(GREEN);
     canvas.printf("%d", button);
-    canvas.setTextColor(WHITE);
-    canvas.fillRect(5, 200, 150, 40, BLACK);
-    canvas.setCursor(45, 190);
-    canvas.printf("save");
-    canvas.setCursor(25, 210);
-    canvas.printf("config");
-    canvas.setCursor(125, 190);
-    canvas.printf("发射率");
-    canvas.setCursor(150, 210);
-    canvas.printf("+");
-    canvas.setCursor(210, 190);
-    canvas.printf("发射率");
-    canvas.setCursor(245, 210);
-    canvas.printf("-");
-    M5.update();
-    if (M5.BtnA.wasPressed()) {
-        ncir2.setConfig();
-        canvas.setCursor(20, 140);
-        canvas.printf("config saved");
-    } else if (M5.BtnB.wasPressed()) {
-        if (emissivity < 1.0) {
-            emissivity += 0.01;
-            ncir2.setEmissivity(emissivity);
-        }
-    } else if (M5.BtnC.wasPressed()) {
-        if (emissivity > 0.1) {
-            emissivity -= 0.01;
-            ncir2.setEmissivity(emissivity);
-        }
-    }
 
     canvas.pushSprite(0, 0);
 }
